6add5d83c431e816cc64f7d9096cba78008f6832
sanych-sun <alexes.polyakov@gmail.com>
2020-08-19 00:12:04 -0700

    Initial commit

---
+++ src/Prometheus.Client.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Prometheus.Client.Abstractions;
+using Prometheus.Client.Collectors;
+using Prometheus.Client.Collectors.Abstractions;
+
+namespace Prometheus.Client.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddMetricFactory(this IServiceCollection services)
+        {
+            services.AddSingleton<ICollectorRegistry, CollectorRegistry>();
+            services.AddSingleton<IMetricFactory, MetricFactory>();
+        }
+    }
+}
