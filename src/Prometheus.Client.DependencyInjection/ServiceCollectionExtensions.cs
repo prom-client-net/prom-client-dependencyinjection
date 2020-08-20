318a534260e818ccc66529a65479ff3f80504be2
sanych-sun <alexes.polyakov@gmail.com>
2020-08-20 02:36:11 -0700

    Add xml docs

--- src/Prometheus.Client.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Prometheus.Client.DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,11 +7,20 @@ namespace Prometheus.Client.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add IMetricFactory and ICollectorRegistry into service collection
+        /// </summary>
+        /// <param name="services">Service collection</param>
         public static void AddMetricFactory(this IServiceCollection services)
         {
             services.AddMetricFactory(new CollectorRegistry());
         }
 
+        /// <summary>
+        /// Add IMetricFactory and ICollectorRegistry into service collection
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        /// <param name="collectorRegistry">Explicit collector registry to use</param>
         public static void AddMetricFactory(this IServiceCollection services, ICollectorRegistry collectorRegistry)
         {
             services.AddSingleton(collectorRegistry);
